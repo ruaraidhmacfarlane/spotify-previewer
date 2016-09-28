66a7f219082950370fde96af829426de41443be1
Ruaraidh Macfarlane <ruaraidh2@gmail.com>
2016-09-28 21:22:14 +0000

    Add search form that alerts query

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-alert('test')
+document.getElementById('search-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert(document.getElementById('query').value);
+}, false);
