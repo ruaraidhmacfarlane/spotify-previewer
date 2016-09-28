91a2b19ad951a03b99f3b3a425cc1fee4e525320
Ruaraidh Macfarlane <ruaraidh2@gmail.com>
2016-09-28 21:52:45 +0000

    Query spotify API and display JSON

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 var resultsPlaceholder = document.querySelector('#results');
 
+var searchArtist = function (query) {
+    $.ajax({
+        url: 'https://api.spotify.com/v1/search',
+        data: {
+            q: query,
+            type: 'artist'
+        },
+        success: function (response) {
+            resultsPlaceholder.innerHTML = JSON.stringify(response, null, 2);
+        }
+    });
+};
+
 document.getElementById('search-form').addEventListener('submit', function (event) {
     event.preventDefault();
-    resultsPlaceholder.innerHTML = document.getElementById('query').value;
+    searchArtist(document.getElementById('query').value);
 }, false);
-
